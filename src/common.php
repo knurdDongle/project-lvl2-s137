c61d5417d701f27d7df561fe458e6a876b382604
ramazan <yaru@bk.ru>
2017-08-08 14:21:26 +0300

    add findDiff()

--- src/common.php
+++ src/common.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ramazan
- * Date: 07.08.17
- * Time: 12:15
- */
+namespace Common;
+
+const DOC = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  --format <fmt>                Report format [default: pretty]
+DOC;
+
+function startGenDiff()
+{
+    $args = \Docopt::handle(DOC);
+
+//    foreach ($args as $k=>$v)
+//        echo $k.': '.json_encode($v).PHP_EOL;
+
+    if ($args['<firstFile>']) {
+        $firstFile = $args['<firstFile>'];
+        $secondFile = $args['<secondFile>'];
+        \DiffFinder\findDiff($firstFile, $secondFile);
+    }
+}
--- src/diffFinder.php
+++ src/diffFinder.php
@@ -1,7 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ramazan
- * Date: 07.08.17
- * Time: 12:10
- */
+
+namespace DiffFinder;
+
+function findDiff($firstFile, $secondFile)
+{
+    $firstFileArray = json_decode(file_get_contents($firstFile), true);
+    $secondFileArray = json_decode(file_get_contents($secondFile), true);
+
+    $resultArray = [];
+
+    $intersection = array_intersect_assoc($firstFileArray, $secondFileArray);
+    $diffArray1 = array_diff_assoc($firstFileArray, $secondFileArray);
+    $diffArray2 = array_diff_assoc($secondFileArray, $firstFileArray);
+
+    $resultArray = mergeDiff($resultArray, $diffArray1, '+');
+    $resultArray = mergeDiff($resultArray, $diffArray2, '-');
+    $resultArray = mergeDiff($resultArray, $intersection, ' ');
+
+    uksort($resultArray, "\DiffFinder\cmp");
+
+    $resultString = arrayToText($resultArray);
+
+    printResultString($resultString);
+}
+
+
+function mergeDiff($res, $diff, $sign)
+{
+    foreach ($diff as $key => $value) {
+        $res["$sign $key"] = $value;
+    }
+    return $res;
+}
+
+
+function cmp($key1, $key2)
+{
+    $key1 = strstr($key1, ' ');
+    $key2 = strstr($key2, ' ');
+    if ($key1 == $key2) {
+        return 0;
+    }
+    return ($key1 < $key2) ? -1 : 1;
+}
+
+
+function arrayToText($array)
+{
+    return implode("\n", array_map(function ($key, $value) {
+        return "  $key: $value";
+    }, array_keys($array), $array));
+}
+
+
+function printResultString($resultString)
+{
+    echo "{\n".$resultString."\n}\n";
+}
