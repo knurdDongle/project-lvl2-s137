fcd59f6a08aa126795d42474c5b4fa61d6fba945
ramazan <yaru@bk.ru>
2017-08-09 10:55:19 +0300

    edit tests

--- tests/diffFinderTest.php
+++ tests/diffFinderTest.php
@@ -7,7 +7,13 @@ class DiffFinderTest extends \PHPUnit_Framework_TestCase
 {
     public function testFindDiff()
     {
-        $findDiffResult = "{\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}";
+        $findDiffResult = "{
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}";
         $this->assertEquals("$findDiffResult", \DiffFinder\findDiff('tests/before.json', 'tests/after.json'));
     }
 }
