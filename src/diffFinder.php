e89f85058981ac0ee6ce52b8cdea01a4160ca52f
ramazan <yaru@bk.ru>
2017-08-12 16:05:41 +0300

    edit arraysDiff() in common.php

--- src/diffFinder.php
+++ src/diffFinder.php
@@ -6,7 +6,7 @@ use function \Funct\Collection\union;
 
 function findDiff($array1, $array2)
 {
-    $resultArray = arraysDiff($array1, $array2);
+    $resultArray = arraysDiff2($array1, $array2);
 
     return arrayToText($resultArray);
 }
@@ -22,7 +22,7 @@ function boolToText($key)
     return $key;
 }
 
-
+/*
 function arraysDiff($array1, $array2)
 {
     $unionArraysKeys = union(array_keys($array1), array_keys($array2));
@@ -43,27 +43,23 @@ function arraysDiff($array1, $array2)
         $acc["  - $key"] = $array1[$key];
         return $acc;
     }, []);
-}
+}*/
 
 
 function arrayToText($array, $spaces = '')
 {
     $result = implode("\n", array_map(function ($key, $value) use ($spaces) {
-//        if (is_array($value)) {
-//            return arrayToText($value, '');
-//        }
         $value = boolToText($value);
         return "$spaces$key: $value";
     }, array_keys($array), $array));
 
-    return "{\n$result\n}\n";
+    return "{\n$result\n}";
 }
 
-/*
+
 function arraysDiff2($array1, $array2)
 {
     $unionArraysKeys = union(array_keys($array1), array_keys($array2));
-//    print_r($unionArraysKeys);
 
     return array_reduce($unionArraysKeys, function ($acc, $key) use ($array1, $array2) {
         if (array_key_exists($key, $array1) && array_key_exists($key, $array2)) {
@@ -71,7 +67,7 @@ function arraysDiff2($array1, $array2)
                 $acc["    $key"] = $array1[$key];
                 return $acc;
             } elseif (is_array($array1[$key])) {
-                $acc["    $key"] = arrayToJSON(arraysDiff2($array1[$key], $array2[$key]), '    ');
+                $acc["    $key"] = arrayToText(arraysDiff2($array1[$key], $array2[$key]), '    ');
                 return $acc;
             } else {
                 $acc["  + $key"] = $array2[$key];
@@ -80,14 +76,14 @@ function arraysDiff2($array1, $array2)
             }
         } elseif (array_key_exists($key, $array2) && !array_key_exists($key, $array1)) {
             if (is_array($array2[$key])) {
-                $acc["  + $key"] = arrayToJSON($array2[$key], '        ');
+                $acc["  + $key"] = arrayToText($array2[$key], '        ');
                 return $acc;
             }
             $acc["  + $key"] = $array2[$key];
             return $acc;
         }
         if (is_array($array1[$key])) {
-            $acc["  - $key"] = arrayToJSON($array1[$key], '        ');
+            $acc["  - $key"] = arrayToText($array1[$key], '        ');
             return $acc;
         }
         $acc["  - $key"] = $array1[$key];
@@ -100,4 +96,3 @@ function arrayToJSON($array)
 {
     return json_encode($array, JSON_PRETTY_PRINT);
 }
-*/
