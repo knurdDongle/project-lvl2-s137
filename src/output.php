f2e1fc4c8abcbd8beeb3649c7b225e3c81b81933
ramazan <yaru@bk.ru>
2017-08-23 10:29:23 +0300

    edit boolToText()

--- src/output.php
+++ src/output.php
@@ -2,60 +2,6 @@
 
 namespace DiffFinder\output;
 
-/*function output(array $AST, $spaces = '')
-{
-    $line = '';
-
-    foreach ($AST as $array) {
-        if ($array['type'] === 'nested') {
-            $children = output($array['children'], '  ');
-            $line .= "    \"{$array['key']}\": {\n$children    }\n";
-        } elseif ($array['type'] === 'unchanged') {
-            $value = boolToText($array['from']);
-            $line .= "$spaces$spaces$spaces  \"{$array['key']}\": {$value}\n";
-        } elseif ($array['type'] === 'removed') {
-            if (is_array($array['from'])) {
-                $from = unpackArray($array['from'], $spaces);
-                $line .= "$spaces$spaces  - \"{$array['key']}\": {\n{$from}$spaces$spaces    }\n";
-            } else {
-                $value = boolToText($array['from']);
-                $line .= "$spaces$spaces$spaces- \"{$array['key']}\": {$value}\n";
-            }
-        } elseif ($array['type'] === 'added') {
-            if (is_array($array['from'])) {
-                $from = unpackArray($array['from'], $spaces);
-                $line .= "$spaces$spaces  + \"{$array['key']}\": {\n{$from}$spaces$spaces    }\n";
-            } else {
-                $value = boolToText($array['from']);
-                $line .= "$spaces$spaces$spaces+ \"{$array['key']}\": {$value}\n";
-            }
-        } elseif ($array['type'] === 'changed') {
-//            if (is_array($array['from'])) {
-//                $from = unpackArray($array['from']);
-//                $line .= "$spaces+ \"{$array['key']}\": {\n{$from}}\n";
-//            } else {
-            $value1 = boolToText($array['from']);
-            $value2 = boolToText($array['to']);
-            $line .= "$spaces$spaces$spaces- \"{$array['key']}\": {$value1}\n";
-            $line .= "$spaces$spaces$spaces+ \"{$array['key']}\": {$value2}\n";
-//            }
-        }
-    }
-
-    return $line;
-}
-
-
-function unpackArray($array, $spaces) {
-    return array_reduce(array_keys($array), function ($acc, $key) use ($array, $spaces) {
-        if ($array[$key] !== null) {
-            $acc .= "$spaces$spaces        \"{$key}\": \"{$array[$key]}\"\n";
-            return $acc;
-        }
-    }, '');
-}*/
-
-
 function boolToText($value)
 {
     if ($value === true) {
@@ -63,22 +9,8 @@ function boolToText($value)
     } elseif ($value === false) {
         return 'false';
     }
-    return "\"$value\"";
+    return $value;
 }
-
-/*function unpackArray($array, $depth) {
-    $spaces = str_repeat(' ', $depth * 4 + 6);
-
-    return array_reduce(array_keys($array), function ($acc, $key) use ($array, $depth, $spaces) {
-        if (is_array($array[$key])) {
-            $value = unpackArray($array[$key], $depth + 1);
-            $acc .= "$spaces  {$key}:\n{$value}";
-            return $acc;
-        }
-        $acc .= "$spaces  {$key}: {$array[$key]}\n";
-        return $acc;
-    }, '');
-}*/
 
 
 function output($AST, $depth = 0)
@@ -128,7 +60,7 @@ function buildLine($isNested, $spaces, $mark, $key, $value)
         $half2 = "{\n{$value}$spaces  }\n";
     } else {
         $value = boolToText($value);
-        $half2 = "{$value}\n";
+        $half2 = ($value !== 'true' && $value !== 'false') ? "\"{$value}\"\n" : "{$value}\n";
     }
 
     return $half1 . $half2;
@@ -146,7 +78,7 @@ function outputPlain($AST, $parents = '')
             } elseif ($array['changeType'] === 'removed') {
                 $result .= buildLinePlain('removed', $parents . $array['key']);
             } elseif ($array['changeType'] === 'added') {
-                $result .= buildLinePlain('added', $parents . $array['key'], '"complex value"');
+                $result .= buildLinePlain('added', $parents . $array['key'], 'complex value');
             }
         } else {
             if ($array['changeType'] === 'changed') {
@@ -162,6 +94,7 @@ function outputPlain($AST, $parents = '')
     return $result;
 }
 
+
 function buildLinePlain($changeType, $property, $value1 = '', $value2 = '')
 {
     $line = '';
@@ -171,9 +104,9 @@ function buildLinePlain($changeType, $property, $value1 = '', $value2 = '')
     if ($changeType === 'removed') {
         $line = "'$property' was $changeType";
     } elseif ($changeType === 'added') {
-        $line = "'$property' was $changeType with value: $value1";
+        $line = "'$property' was $changeType with value: '$value1'";
     } elseif ($changeType === 'changed') {
-        $line = "'$property' was $changeType. From $value1 to $value2";
+        $line = "'$property' was $changeType. From '$value1' to '$value2'";
     }
 
     return "Property $line\n";
